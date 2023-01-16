c2ef640c38ca712d62eb4514f631d221e03373d7
Alejandra Guerin <79866676+TimboAG@users.noreply.github.com>
2023-01-16 12:27:43 -0300

    usuarios

--- src/main/java/com/peliculas/peliculas/excepciones/GlobalException.java
+++ src/main/java/com/peliculas/peliculas/excepciones/GlobalException.java
@@ -1,17 +1,23 @@
-
 package com.peliculas.peliculas.excepciones;
 
 import com.peliculas.peliculas.entidad.Message;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
+@RestControllerAdvice
 public class GlobalException {
-      @ExceptionHandler(Exception.class)
+
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<Message> throwGeneralException(Exception ex) {
         return ResponseEntity.internalServerError()
                 .body(new Message(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage()));
     }
 
+    @ExceptionHandler(MiException.class)
+    public ResponseEntity<Message> throwMiException(MiException ex) {
+        return ResponseEntity.internalServerError()
+                .body(new Message(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage()));
+    }
 }
